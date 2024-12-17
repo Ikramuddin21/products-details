5f5f724dc558b0e01a105bd0784eee6c360c7c38
ikramuddin21 <shaonakhand2122@gmail.com>
2024-12-17 09:12:48 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const colorItems = document.querySelectorAll(".brand-color");
+const openModalButton = document.getElementById("openModalButton");
+const closeModalButton = document.getElementById("closeModalButton");
+const modal = document.getElementById("myModal");
 
 colorItems.forEach((item) => {
   item.addEventListener("click", () => {
@@ -9,4 +12,21 @@ colorItems.forEach((item) => {
     // Add "selected" style to the clicked item
     item.classList.add("ring-2", "ring-offset-2", "ring-offset-white");
   });
+});
+
+// open modal
+openModalButton.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+});
+
+// close modal
+closeModalButton.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
+
+// close modal when clicking outside the modal content
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.classList.add("hidden");
+  }
 });
